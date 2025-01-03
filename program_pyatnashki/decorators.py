785ecdae96f0b6372c079f5f5c9989271a0b7ff8
kateswer <katepik2007@gmail.com>
2025-01-03 20:06:29 +0300

    Update decorators.py

--- program_pyatnashki/decorators.py
+++ program_pyatnashki/decorators.py
@@ -1,1 +1,12 @@
+"""Файл, содержащий декоратор для логирования вызовов методов."""
+import functools
 
+def log_method_calls(method):
+    """Декоратор, который логирует вызовы методов."""
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        print(f"Вызван метод: {method.__name__}, аргументы: {args}, {kwargs}")
+        result = method(self, *args, **kwargs)
+        print(f"Результат: {result}")
+        return result
+    return wrapper
