ec4ee5babc0deb06a1d4c1dc2002a736f6b398fa
kateswer <katepik2007@gmail.com>
2025-01-03 20:06:10 +0300

    Update main.py

--- program_pyatnashki/main.py
+++ program_pyatnashki/main.py
@@ -1,1 +1,15 @@
+"""Основной файл для запуска игры """
+import pygame
+from Class_puzzle_game import PuzzleGame
 
+if __name__ == "__main__":
+    try:
+        grid_size = int(input("Выберите уровень игры (3 или 4): "))# Запрос уровня игры у пользователя.
+    except KeyboardInterrupt:
+        print("\nВвод прерван. Завершение программы.")
+        exit()# Завершение программы при прерывании.
+
+    # Создание игры с выбранным размером сетки.
+    game = PuzzleGame(grid_size)
+    game.run()
+    pygame.quit()
